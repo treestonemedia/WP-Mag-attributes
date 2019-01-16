9d8ad11e988475b673503da7cacd354472d32241
treestonemedia <info@treestonemedia.com>
2019-01-16 17:50:54 +0000

    Apply fixes from StyleCI

--- api/api.php
+++ api/api.php
@@ -7,12 +7,12 @@
  * Time: 8:50 PM.
  */
 defined('ABSPATH') or die('Cannot access pages directly.'); //protect from direct access
-class magento {
-
+class magento
+{
     public $sessionId;
 
- public function connect()
-{
+    public function connect()
+    {
         $mg_host = get_option('mg_url'); //get the magento shop URL as set in settings
         $mg_usr = get_option('mg_api_user'); //get the magento api user as set in settings
         $mg_scrt = get_option('mg_scrt'); //get the magento api secret as set in settings
