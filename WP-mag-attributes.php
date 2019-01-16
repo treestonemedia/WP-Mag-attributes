134a938a9bf68b35dfbccea644e791d97425b059
treestonemedia <info@treestonemedia.com>
2019-01-16 15:52:46 +0000

    Apply fixes from StyleCI

--- WP-mag-attributes.php
+++ WP-mag-attributes.php
@@ -10,97 +10,84 @@ Author URI: http://treestone.com
 License: GPL2
 */
 
-defined( 'ABSPATH' ) or die( "Cannot access pages directly." ); //protect from direct access
+defined('ABSPATH') or die('Cannot access pages directly.'); //protect from direct access
 
-define( 'MAGENTO__PLUGIN_DIR', plugin_dir_path( __FILE__ ) ); //define the current path
-require_once( MAGENTO__PLUGIN_DIR . 'api/api.php' ); // load the api class
-require_once( MAGENTO__PLUGIN_DIR . 'admin/magento-admin.php' ); //load the admin settings
+define('MAGENTO__PLUGIN_DIR', plugin_dir_path(__FILE__)); //define the current path
+require_once MAGENTO__PLUGIN_DIR.'api/api.php'; // load the api class
+require_once MAGENTO__PLUGIN_DIR.'admin/magento-admin.php'; //load the admin settings
 
+function magento_attributes_func($atts)
+{
+    $id = $atts['attribute_id']; //get the attribute id from the shortcode
 
-function magento_attributes_func( $atts ) {
+    $transientUnique = 'ts_magento_api'.$id; //set a unique transient
 
-	$id = $atts['attribute_id']; //get the attribute id from the shortcode
+    $transient = get_transient($transientUnique); //check if the transient is already cached
 
-	$transientUnique = 'ts_magento_api' . $id; //set a unique transient
+    if (!empty($transient)) {
 
-	$transient = get_transient( $transientUnique ); //check if the transient is already cached
+        // The function will return here every time after the first time it is run, until the transient expires.
+        return $transient;
 
-	if ( ! empty( $transient ) ) {
+    // Nope!  We gotta make a call.
+    } else {
+        $mg = new magento(); //init magento api class
 
-		// The function will return here every time after the first time it is run, until the transient expires.
-		return $transient;
+        $attributes = $mg->getAttributeOptions($id); //init magento api call with attribute id
 
-		// Nope!  We gotta make a call.
-	} else {
+        $return = '<ul>'; //start a list for the results, can be set as table also
 
-		$mg = new magento(); //init magento api class
+        foreach ($attributes as $value) {
+            if (strlen($value->label) > 2) { //had to add this is because of some blank attribute option values
 
-		$attributes = $mg->getAttributeOptions( $id ); //init magento api call with attribute id
+                $return .= '<li>'.$value->label.'</li>'; //get only the label of the attribute option
+            }
+        }
 
-		$return = "<ul>"; //start a list for the results, can be set as table also
+        $return .= '</ul>'; //close the ul
 
-		foreach ( $attributes as $value ) {
+        set_transient($transientUnique, $return, 86400); //if the transient wasn't cached, we just did :)
 
-			if ( strlen( $value->label ) > 2 ) { //had to add this is because of some blank attribute option values
-
-				$return .= "<li>" . $value->label . "</li>"; //get only the label of the attribute option
-			}
-
-		}
-
-		$return .= "</ul>"; //close the ul
-
-		set_transient( $transientUnique, $return, 86400 ); //if the transient wasn't cached, we just did :)
-
-
-		return $return;
-	}
-
+        return $return;
+    }
 }
 
-add_shortcode( "magento_attributes", "magento_attributes_func" ); //this enables the shortcode
+add_shortcode('magento_attributes', 'magento_attributes_func'); //this enables the shortcode
 
 //Add Sales Orders
 
-function magento_sales_func() {
+function magento_sales_func()
+{
+    $mg = new magento(); //init magento api class
+    $orders = $mg->getOrdersList(); //init magento api call with orders
+    foreach ($orders as $order) {
+        $return = '<pre>'; //open a 'pre' for each order
+        foreach ($order as $name => $value) {
+            $return .= '<b>'.$name.' : </b>'.$value.'<br/>';
+        }
+        $return .= '</pre>'; //close the 'pre'
+    }
 
-	$mg     = new magento(); //init magento api class
-	$orders = $mg->getOrdersList(); //init magento api call with orders
-	foreach ( $orders as $order ) {
-
-		$return = "<pre>"; //open a 'pre' for each order
-		foreach ( $order as $name => $value ) {
-
-			$return .= "<b>" . $name . " : </b>" . $value . "<br/>";
-
-
-		}
-		$return .= "</pre>"; //close the 'pre'
-	}
-
-	return $return;
+    return $return;
 }
 
-add_shortcode( "magento_sales", "magento_sales_func" ); //this enables the shortcode
+add_shortcode('magento_sales', 'magento_sales_func'); //this enables the shortcode
 
 //Add store info
 
-function magento_info() {
+function magento_info()
+{
+    $mg = new magento(); //init magento api class
+    $storeInfo = $mg->getStoreInfo(); //init magento api class with store info
 
-	$mg        = new magento(); //init magento api class
-	$storeInfo = $mg->getStoreInfo(); //init magento api class with store info
+    $return = '<pre>';
 
-	$return = "<pre>";
+    foreach ($storeInfo as $k => $v) {
+        $return .= '<b>'.$k.' :</b> '.$v.'<br/>';
+    }
+    $return .= '</pre>';
 
-	foreach ( $storeInfo as $k => $v ) {
-
-		$return .= "<b>" . $k . " :</b> " . $v . "<br/>";
-	}
-	$return .= "</pre>";
-
-	return $return;
-
+    return $return;
 }
 
-
-add_shortcode( "magento_store_info", "magento_info" ); //this enables the shortcode
+add_shortcode('magento_store_info', 'magento_info'); //this enables the shortcode
--- api/api.php
+++ api/api.php
@@ -3,78 +3,64 @@
  * Created by PhpStorm.
  * User: info
  * Date: 1/28/2018
- * Time: 8:50 PM
+ * Time: 8:50 PM.
  */
+defined('ABSPATH') or die('Cannot access pages directly.'); //protect from direct access
+class magento
+{
+    public $sessionId;
 
-defined( 'ABSPATH' ) or die( "Cannot access pages directly." ); //protect from direct access
-class magento {
+    public function connect()
+    {
+        $mg_host = get_option('mg_url'); //get the magento shop URL as set in settings
+        $mg_usr = get_option('mg_api_user'); //get the magento api user as set in settings
+        $mg_scrt = get_option('mg_scrt'); //get the magento api secret as set in settings
 
-	public $sessionId;
+        $proxy = new SoapClient($mg_host.'/api/v2_soap/?wsdl'); //initiate request to magento
 
+        $this->sessionId = $proxy->login($mg_usr, $mg_scrt); //login to magento with API credentials
 
-	public function connect() {
+        return $proxy;
+    }
 
+    public function getAttributeOptions($id)
+    {
+        $attribute_id = $id; //get the attribute id passed by the shortcode
+        $mg = $this->connect();
 
-		$mg_host = get_option( 'mg_url' ); //get the magento shop URL as set in settings
-		$mg_usr  = get_option( 'mg_api_user' ); //get the magento api user as set in settings
-		$mg_scrt = get_option( 'mg_scrt' ); //get the magento api secret as set in settings
+        $result = $mg->catalogProductAttributeOptions($this->sessionId, $attribute_id); //get the attributes options
 
+        return $result;
+    }
 
-		$proxy = new SoapClient( $mg_host . '/api/v2_soap/?wsdl' ); //initiate request to magento
+    //start order section
+    //https://www.fontis.com.au/blog/web-services-api-filter-operators
 
-		$this->sessionId = $proxy->login( $mg_usr, $mg_scrt ); //login to magento with API credentials
+    public function getOrdersList()
+    {
+        $params = [
+            'complex_filter' => [
+                    [
+                        'key'   => 'created_at',
+                        'value' => ['key' => 'from', 'value' => '2018-01-01 01:01:01'],
+                    ],
+                    ['key' => 'status ', 'value' => 'Processing'],
 
-		return $proxy;
+                ],
 
+        ];
+        $mg = $this->connect();
+        $result = $mg->salesOrderList($this->sessionId, $params);
 
-	}
+        return $result;
+    }
 
-	public function getAttributeOptions( $id ) {
+    public function getStoreInfo()
+    {
+        $mg = $this->connect();
+        $result = $mg->magentoInfo($this->sessionId);
 
-		$attribute_id = $id; //get the attribute id passed by the shortcode
-		$mg           = $this->connect();
-
-		$result = $mg->catalogProductAttributeOptions( $this->sessionId, $attribute_id ); //get the attributes options
-
-		return $result;
-
-
-	}
-
-	//start order section
-	//https://www.fontis.com.au/blog/web-services-api-filter-operators
-
-	public function getOrdersList() {
-
-		$params = array(
-			'complex_filter' =>
-				array(
-					array(
-						'key'   => 'created_at',
-						'value' => array( 'key' => 'from', 'value' => '2018-01-01 01:01:01' )
-					),
-					array( 'key' => 'status ', 'value' => 'Processing' ),
-
-				),
-
-		);
-		$mg     = $this->connect();
-		$result = $mg->salesOrderList( $this->sessionId, $params );
-
-
-		return $result;
-
-	}
-
-	public function getStoreInfo() {
-
-		$mg     = $this->connect();
-		$result = $mg->magentoInfo( $this->sessionId );
-
-		//var_dump($result);
-		return $result;
-
-
-	}
-
+        //var_dump($result);
+        return $result;
+    }
 }
